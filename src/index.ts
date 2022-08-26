d37a41e63a71fc97974e588fa6b819def8e4c18a
KoJem9Ka <bis21080000@gmail.com>
2022-08-27 02:30:19 +0300

    fixes for project start

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import express          from 'express'
 import { PrismaClient } from '../prisma/client'
 import { activateBot }  from './bot'
 
@@ -7,3 +8,22 @@ import { activateBot }  from './bot'
 console.clear()
 export const prisma = new PrismaClient()
 void activateBot()
+
+
+const app = express()
+const port = 3000
+
+app.get( '/', ( req, res ) => {
+  res.json( { 'message': 'ok' } )
+} )
+// @ts-ignore
+app.use( ( err, req, res, next ) => {
+  const statusCode = err.statusCode || 500
+  console.error( err.message, err.stack )
+  res.status( statusCode ).json( { 'message': err.message } )
+
+  return
+} )
+app.listen( port, '0.0.0.0', () => {
+  console.log( `Example app listening at http://localhost:${port}` )
+} )
